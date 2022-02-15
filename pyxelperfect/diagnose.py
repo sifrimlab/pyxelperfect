d3562173cf2d386a72f9aa33896240b7b03a6588
WoutDavid <wouters.david@hotmail.com>
2022-02-15 11:08:25 +0100

    add combined stats funciton

--- pyxelperfect/diagnose.py
+++ pyxelperfect/diagnose.py
@@ -1,13 +1,36 @@
+import os
+from functools import reduce
 from skimage import io
 import numpy as np
 import pandas as pd
+import glob
 from skimage.filters import laplace
 from skimage.util import dtype_limits
 from segment import otsuThreshold
+from typing import List
 
+import PIL # this is needed to read in the giant images with skimage.io.read_collection
+PIL.Image.MAX_IMAGE_PIXELS = 933120000
 
+def getImageStats(image: np.ndarray, out_print: bool = False, save: bool = False, result_prefix: str = "results") -> pd.DataFrame:
+    """Returns general stats about the input image.
 
-def getImageStats(image: np.ndarray, save: bool = False,out_print: bool = False) -> pd.DataFrame:
+    Parameters
+    ----------
+    image : np.ndarray
+        input image
+    out_print : bool
+        Determines whether to print out the results to stdout, default = False
+    save : bool
+        Determines whether to save the results to a csv, default = False
+    result_prefix : str
+        prefix to use when saving the csv: output file name will be {result_prefix}.csv
+
+    Returns
+    -------
+    pd.DataFrame
+
+    """
     attribute_col = []
     value_col = []
 
@@ -19,6 +42,9 @@ def getImageStats(image: np.ndarray, save: bool = False,out_print: bool = False)
 
     attribute_col.append("Dtype range")
     value_col.append(dtype_limits(image))
+
+    attribute_col.append("Actual range")
+    value_col.append((np.amin(image), np.amax(image)))
 
     attribute_col.append("Mean")
     value_col.append(np.mean(image))
@@ -46,14 +72,53 @@ def getImageStats(image: np.ndarray, save: bool = False,out_print: bool = False)
     attribute_col.append("Percentage above otsu")
     value_col.append(perc_above)
 
+    # Make result dataframe
     zipped = list(zip(attribute_col, value_col))
-    result_df = pd.DataFrame(zipped, columns=['Attribute', 'Value'], index=None)
+    result_df = pd.DataFrame(zipped, columns=['Attribute', f'{result_prefix}'], index=None)
+
     if out_print:
         print(result_df.to_markdown())
+    if save:
+        result_df.to_csv(f"{result_prefix}.csv", index=None)
+    return result_df
+
+def compareImageStats(glob_pattern: str = None, result_prefix = "result") -> pd.DataFrame:
+    """Create a dataframe comparing the general stats of a collection of images, defined by a glob pattern.
+
+    Parameters
+    ----------
+    glob_pattern : str
+        glob_pattern
+    result_prefix :
+        prefix to use when saving the csv: output file name will be {result_prefix}.html
+
+    Returns
+    -------
+    pd.DataFrame
+
+    """
+    # if glob_pattern is None and image_list is None:
+    #     raise TypeError("Function requires either glob_pattern or image_list to be given, not both.") 
+    
+    
+    name_image_dict = {os.path.basename(file): io.imread(file) for file in glob.glob(glob_pattern)}
+    # image_list = io.imread_collection(glob_pattern)
+
+    df_list  = []
+    for k,v in name_image_dict.items():
+        df_list.append(getImageStats(v, result_prefix = k, save=False, out_print=False))
+
+    merged_df = reduce(lambda x, y: pd.merge(x, y, on = 'Attribute'), df_list)
+    merged_df.to_html(f"{result_prefix}.html")
+
+        
+
 
 if __name__ == '__main__':
-    image = io.imread("/home/david/Documents/vsn-pipelines/starfish_data/iss/Round2/Round2_c1.TIF")
-    getImageStats(image, out_print = True)
+    # image = io.imread("/media/tool/enteric_neurones/slidescanner_examples/Good/processed_Slide2-2-2_Region0000_Channel647,555,488_Seq0017/Slide2-2-2_Region0000_Channel647,555,488_Seq0017_c1_z0_tile14.tif")
+    # getImageStats(image, out_print = True)
+    # image_list = [io.imread(file) for file in ("/media/tool/enteric_neurones/slidescanner_examples/Good/processed_Slide2-2-2_Region0000_Channel647,555,488_Seq0017/Slide2-2-2_Region0000_Channel647,555,488_Seq0017_c1_z0_tile4.tif", "/media/tool/enteric_neurones/slidescanner_examples/Good/processed_Slide2-2-2_Region0000_Channel647,555,488_Seq0017/Slide2-2-2_Region0000_Channel647,555,488_Seq0017_c1_z0_tile5.tif")]
+    compareImageStats(glob_pattern = "/media/tool/enteric_neurones/slidescanner_examples/Good/processed_Slide2-2-2_Region0000_Channel647,555,488_Seq0017/*tile*.tif")
 
 
     
