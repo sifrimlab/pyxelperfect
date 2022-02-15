8597a8fd76551b27a8ef3e0bfd51eaa08d77b7ca
WoutDavid <wouters.david@hotmail.com>
2022-02-15 17:26:48 +0100

    create automatic enhacement of contrast and brightness

--- pyxelperfect/manipulate.py
+++ pyxelperfect/manipulate.py
@@ -1,51 +1,56 @@
 import cv2
-from skimage import io
-import matplotlib.pyplot as plt
 
-def controller(img, brightness=255, contrast=127):
-    brightness = int((brightness - 0) * (255 - (-255)) / (510 - 0) + (-255))
- 
-    contrast = int((contrast - 0) * (127 - (-127)) / (254 - 0) + (-127))
- 
-    if brightness != 0:
- 
-        if brightness > 0:
- 
-            shadow = brightness
- 
-            max = 255
- 
-        else:
- 
-            shadow = 0
-            max = 255 + brightness
- 
-        al_pha = (max - shadow) / 255
-        ga_mma = shadow
- 
-        # The function addWeighted
-        # calculates the weighted sum
-        # of two arrays
-        cal = cv2.addWeighted(img, al_pha,
-                              img, 0, ga_mma)
- 
-    else:
-        cal = img
- 
-    if contrast != 0:
-        Alpha = float(131 * (contrast + 127)) / (127 * (131 - contrast))
-        Gamma = 127 * (1 - Alpha)
- 
-        # The function addWeighted calculates
-        # the weighted sum of two arrays
-        cal = cv2.addWeighted(cal, Alpha,
-                              cal, 0, Gamma)
- 
-    # putText renders the specified
-    # text string in the image.
-    return cal
-if __name__ == "__main__":
-    image = io.imread("/home/nacho/Documents/communISS/data/merfish/merfish_5.tif")
-    cal = controller(image)
-    plt.hist(cal)
+# Automatic brightness and contrast optimization with optional histogram clipping
+def automaticBrightnessAndContrast(image, clip_hist_percent=1):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Calculate grayscale histogram
+    hist = cv2.calcHist([gray],[0],None,[256],[0,256])
+    hist_size = len(hist)
+    
+    # Calculate cumulative distribution from the histogram
+    accumulator = []
+    accumulator.append(float(hist[0]))
+    for index in range(1, hist_size):
+        accumulator.append(accumulator[index -1] + float(hist[index]))
+    
+    # Locate points to clip
+    maximum = accumulator[-1]
+    clip_hist_percent *= (maximum/100.0)
+    clip_hist_percent /= 2.0
+    
+    # Locate left cut
+    minimum_gray = 0
+    while accumulator[minimum_gray] < clip_hist_percent:
+        minimum_gray += 1
+    
+    # Locate right cut
+    maximum_gray = hist_size -1
+    while accumulator[maximum_gray] >= (maximum - clip_hist_percent):
+        maximum_gray -= 1
+    
+    # Calculate alpha and beta values
+    alpha = 255 / (maximum_gray - minimum_gray)
+    beta = -minimum_gray * alpha
+    
+    '''
+    # Calculate new histogram with desired range and show histogram 
+    new_hist = cv2.calcHist([gray],[0],None,[256],[minimum_gray,maximum_gray])
+    plt.plot(hist)
+    plt.plot(new_hist)
+    plt.xlim([0,256])
     plt.show()
+    '''
+
+    auto_result = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+    return (auto_result, alpha, beta)
+
+if __name__ == "__main__":
+
+    image = cv2.imread("/home/david/Documents/communISS/data/merfish/merfish_5.tif")
+    auto_result, alpha, beta = automaticBrightnessAndContrast(image)
+    print('alpha', alpha)
+    print('beta', beta)
+    cv2.imwrite("/home/david/Documents/communISS/data/merfish/merfish_5_contrast.tif", auto_result)
+    # cv2.imshow('auto_result', auto_result)
+    # cv2.waitKey()
