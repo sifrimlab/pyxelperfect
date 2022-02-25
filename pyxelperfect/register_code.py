28ecf004f7d0f9fbef10199ecd0065619ac1f9ac
WoutDavid <wouters.david@hotmail.com>
2022-02-25 18:04:37 +0100

    add segment crop code

--- pyxelperfect/register_code.py
+++ pyxelperfect/register_code.py
@@ -1,12 +1,22 @@
 import os
 import pandas as pd
+import cv2 as cv
+import numpy as np
 from skimage import io
 from skimage.color import rgb2gray
-from skimage.util import invert
+from skimage.util import invert,img_as_ubyte
 import matplotlib.pyplot as plt
 from manipulate import equalizeImageSize
 from register import performRegistration
 from measure import measureLabeledImage
+
+
+
+
+
+
+
+
 
 """
     0: rotate
@@ -18,7 +28,7 @@ from measure import measureLabeledImage
     6: make overlays?
 """
 
-def cutBboxFromImage(bbox: Tuple, image: np.ndarray):
+def cutBboxFromImage(bbox, image: np.ndarray):
         top_left = bbox[0]
         bot_right = bbox[1]
         row_min, col_min = [coordinate if coordinate > 0 else 0 for coordinate in top_left]
@@ -30,13 +40,29 @@ def cutBboxFromImage(bbox: Tuple, image: np.ndarray):
 
 def isolateForeground(input_image: np.array):
     # create labeled image
+    gray_correct = np.array(255 * (input_image / 255) ** 1.2 , dtype='uint8')
+    # Local adaptative threshold
 
+    thresh = cv.adaptiveThreshold(gray_correct, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 255, 19)
+    thresh = cv.bitwise_not(thresh)
+
+    # # Dilatation et erosion
+    kernel = np.ones((15,15), np.uint8)
+    img_dilation = cv.dilate(thresh, kernel, iterations=1)
+    img_erode = cv.erode(img_dilation,kernel, iterations=1)
+    # # clean all noise after dilatation and erosion
+    img_erode = cv.medianBlur(img_erode, 7)
+
+    ret, labeled_image = cv.connectedComponents(img_erode)
+
+    io.imsave("tmp_labeledimg.tif", labeled_image)
     # Meaure imageprops
     df = measureLabeledImage(labeled_image)
 
     # get max area row
-    image_label = df.iloc[df['Area'].idxmax()]['image_label']
-    image_bbow =  df.iloc[df['Area'].idxmax()]['bbox']
+    print(df.iloc[df['Area'].idxmax()]['image_label'])
+    # image_label = df.iloc[df['Area'].idxmax()]['image_label']
+    # image_bbow =  df.iloc[df['Area'].idxmax()]['bbox']
 
     # convert bbox row to a tuple so that it can serve as input for cutBboxFromImage
 
@@ -75,8 +101,7 @@ if __name__ == '__main__':
     # ref_image_path = "./PWB929_normal_HE_minus_cmc_10X.tif"
     # target_image_path = "./PWB929_DLC1.tif"
     # perform_registration(ref_image_path, target_image_path)
-    df = pd.read_csv("/home/david/Documents/prostate_cancer/cv2labeled.csv")
-    isolateForeground(df)
+    isolateForeground(io.imread("/home/david/Documents/prostate_cancer/testing_data/PWB929_DLC1_grey.tif"))
 
 
 
