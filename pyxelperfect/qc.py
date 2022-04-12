a22730a1aa6e354aa2e4a630c6be2ce9166e417f
WoutDavid <wouters.david@hotmail.com>
2022-04-12 11:42:46 +0200

    add line and circle detectors

--- pyxelperfect/qc.py
+++ pyxelperfect/qc.py
@@ -1,13 +1,61 @@
 import numpy as np
 from skimage import io
 from skimage.transform import resize
+from skimage.feature import canny
+from skimage.filters import sobel
+from skimage.transform import hough_circle, hough_circle_peaks, hough_line, hough_line_peaks
+from skimage.util import img_as_ubyte
 from glob import glob
 from .diagnose import getImageStats
 import re
 import math
 
-def isEdgeImage(image: np.ndarray):
-    return np.amin(image)[0] == 0
+def hasExactlyOneLine(img: np.ndarray, sigma: int = 0, low_threshold:float = None, high_threshold: float=None):
+    """Detects the presence of a vertical or horizontal edge in an image. 
+    To be used when filtering out edge-tiles from a dataset.
+
+    Parameters
+    ----------
+    img : np.ndarray
+        img
+    sigma : int
+        sigma to be used for gaussian blurring by canny edge filter. Default = 0
+        For more documentation, see https://scikit-image.org/docs/dev/api/skimage.feature.html
+    low_threshold : float
+        Low threshold to be used by hysteresis thresholding. 
+        For more documentation, see https://scikit-image.org/docs/dev/api/skimage.feature.html
+    high_threshold : float
+        High threshold to be used by hysteresis thresholding. 
+        For more documentation, see https://scikit-image.org/docs/dev/api/skimage.feature.html
+    """
+
+    edge_image = canny(img, sigma=0, low_threshold = low_threshold, high_threshold = high_threshold)
+    tested_angles = np.array([np.pi, np.pi / 2])
+    h, theta, d = hough_line(edge_image, theta=tested_angles)
+    accums, angles, dists =  hough_line_peaks(h, theta, d)
+    return len(accums) == 1
+
+def hasExactlyOneCircle(img: np.ndarray, min_ydistance:int = 50, min_xdistance:int = 50):
+    """Checks whether the input image contains exactly one circle-like object
+    Note: For this, it is important that the input img is 16bit.
+
+    Parameters
+    ----------
+    img : np.ndarray
+        img
+    min_ydistance : int
+        Min ydistance that two circles to be detected can be apart.
+    min_xdistance : int
+        Min xdistance that two circles to be detected can be apart.
+    """
+
+    sobel_image = sobel(img)
+    sobel_image = img_as_ubyte(sobel_image)
+    hough_radii = np.arange(25, 75, 2)
+    hough_res = hough_circle(sobel_image, hough_radii)
+    accums, cx, cy, radii = hough_circle_peaks(hough_res, hough_radii, min_ydistance = 50, min_xdistance= 50 )
+    return len(radii) == 1
+
 
 def isGoodQuality(image: np.ndarray):
     df = getImageStats(image, out_print=False)
@@ -61,8 +109,6 @@ if __name__ == "__main__":
     st.set_y(0.95)
     fig.subplots_adjust(top=0.85)
     plt.show()
-
-
 
     # Confusion matrix
     # cm = confusion_matrix(y_labs, y_preds, labels=[True, False])
